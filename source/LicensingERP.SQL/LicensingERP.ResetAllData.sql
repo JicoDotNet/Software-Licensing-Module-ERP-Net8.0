86a68fe1fe1b3f78a724649d8f91df56c6c041a4
Soubhik Nandy <nandy.soubhik@live.co.uk>
2024-08-28 15:31:21 +0530

    Sql Query

---
+++ source/LicensingERP.SQL/LicensingERP.ResetAllData.sql
@@ -0,0 +1,95 @@
+TRUNCATE TABLE tbl_client;
+TRUNCATE TABLE tbl_client_category;
+TRUNCATE TABLE tbl_dashboard;
+TRUNCATE TABLE tbl_licence_type;
+TRUNCATE TABLE tbl_license_parameter;
+TRUNCATE TABLE tbl_log_activity;
+TRUNCATE TABLE tbl_log_login;
+TRUNCATE TABLE tbl_mc_data_on_hold;
+TRUNCATE TABLE tbl_menu_group
+TRUNCATE TABLE tbl_menu_list;
+TRUNCATE TABLE tbl_parameters;
+TRUNCATE TABLE tbl_password;
+TRUNCATE TABLE tbl_password_reset_request;
+TRUNCATE TABLE tbl_password_security_questions;
+TRUNCATE TABLE tbl_product;
+TRUNCATE TABLE tbl_product_features;
+TRUNCATE TABLE tbl_product_module;
+TRUNCATE TABLE tbl_request;
+TRUNCATE TABLE tbl_request_acknowledgement;
+TRUNCATE TABLE tbl_request_acknowledgement_docs;
+TRUNCATE TABLE tbl_request_claim;
+TRUNCATE TABLE tbl_request_features;
+TRUNCATE TABLE tbl_request_parameter;
+TRUNCATE TABLE tbl_request_restrict;
+TRUNCATE TABLE tbl_request_status;
+TRUNCATE TABLE tbl_s_state;
+TRUNCATE TABLE tbl_user;
+TRUNCATE TABLE tbl_userdashboard;
+TRUNCATE TABLE tbl_usermenu;
+TRUNCATE TABLE tbl_usertype;
+TRUNCATE TABLE tbl_wf_process;
+TRUNCATE TABLE tbl_wf_process_assign;
+TRUNCATE TABLE tbl_xml_download;
+
+-- ----
+-- Default DANGLING
+-- ----
+INSERT INTO tbl_dashboard (PartialViewName, Description, SortOrder, DivCssClass, IsActive, SessionId, TransactionDate) VALUES
+('GroupWiseUser', 'Group Wise User Graph using Bar Chart', 2, 'col-md-4', 1, 'DEVELOPER', NOW()),
+('Master', 'Master Data Count Value', 1, 'col-md-12', 1, 'DEVELOPER', NOW()),
+('LicenseRequest', 'License wise Request Graph', 3, 'col-md-4', 1, 'DEVELOPER', NOW()),
+('ClientWiseLicenseRequest', 'Client Wise Approved License Request Graph', 4, 'col-md-4', 1, 'DEVELOPER', NOW());
+
+INSERT INTO tbl_menu_group(Icon, DisplayText, IsActive, IsDisplayable, TransactionDate, SessionId) VALUES
+('fa fa-user', 'User Management', 1, True, NOW(), 'DEVELOPER'),
+('fas fa-hourglass', 'Workflow Authoring', 1, True, NOW(), 'DEVELOPER'),
+('fab fa-buromobelexperte', 'Master', 1, True, NOW(), 'DEVELOPER'),
+('fas fa-satellite-dish', 'Requisition', 1, True, NOW(), 'DEVELOPER'),
+('fas fa-chart-bar', 'MIS User Report', 1, True, NOW(), 'DEVELOPER'),
+('fas fa-chart-pie', 'MIS License Report', 1, True, NOW(), 'DEVELOPER');
+
+INSERT INTO tbl_menu_list(MenuGroupId, Icon, DisplayText, Controller, ActionResult, HasRouteId, RouteId, QueryString, HttpType, IsReport, IsActive, IsDisplayable, IsForAdmin, Description, TransactionDate, SessionId, IsForWorkflow) VALUES
+(1, 'fa fa-bezier-curve', 'User Permission', 'User', 'Permission', NULL, NULL, NULL, 'HttpGet', False, True, True, True, 'user permissions are assigned here', NOW(), 'DEVELOPER', False),
+(1, 'fas fa-broadcast-tower', 'Password Change Request', 'User', 'PasswordChangeRequest', NULL, NULL, NULL, 'HttpGet', False, True, True, False, NULL, NOW(), 'DEVELOPER', False),
+(2, 'far fa-circle nav-icon', 'Process', 'Workflow', 'Index', NULL, NULL, NULL, 'HttpGet', False, True, True, False, 'Get different type of Process and add new Process', NOW(), 'DEVELOPER', False),
+(2, 'far fa-circle nav-icon', 'Assign', 'Workflow', 'Assign', NULL, NULL, NULL, 'HttpGet', False, True, True, False, 'Assign different process into different user', NOW(), 'DEVELOPER', False),
+(3, 'far fa-circle', 'User Group', 'User', 'Type', NULL, NULL, NULL, 'HttpGet', False, True, True, False, 'User Group List', NOW(), 'DEVELOPER', False),
+(3, 'far fa-circle', 'User', 'User', 'Index', NULL, NULL, NULL, 'HttpGet', False, True, True, False, 'User page list', NOW(), 'DEVELOPER', False),
+(3, 'far fa-circle', 'License Type', 'License', 'Index', NULL, NULL, NULL, 'HttpGet', False, True, True, False, 'License type List', NOW(), 'DEVELOPER', False),
+(3, 'far fa-circle', 'Client Category', 'Client', 'Category', NULL, NULL, NULL, 'HttpGet', False, True, True, False, 'Client Category add', NOW(), 'DEVELOPER', False),
+(3, 'far fa-circle', 'Client', 'Client', 'Index', NULL, NULL, NULL, 'HttpGet', False, True, True, False, 'Client add page', NOW(), 'DEVELOPER', False),
+(3, 'far fa-circle', 'Product', 'Product', 'Index', NULL, NULL, NULL, 'HttpGet', False, True, True, False, 'Product Add page', NOW(), 'DEVELOPER', False),
+(3, 'far fa-circle', 'Parameter Master', 'Form', 'Manage', NULL, NULL, NULL, 'HttpGet', False, True, True, False, 'Parameter Manage', NOW(), 'DEVELOPER', False),
+(3, 'fas fa-link', 'License Parameter Map', 'License', 'Parameter', NULL, NULL, NULL, 'HttpGet', False, True, True, False, 'Parameter Link page ', NOW(), 'DEVELOPER', False),
+(4, 'far fa-circle', 'New Request', 'Requisition', 'Status', NULL, NULL, NULL, 'HttpGet', False, True, True, False, 'Licence Request For Requisition', NOW(), 'DEVELOPER', False),
+(4, 'far fa-circle', 'Approval Pending', 'Requisition', 'Pending', NULL, NULL, NULL, 'HttpGet', False, True, True, False, 'Requisition Approval screen', NOW(), 'DEVELOPER', False),
+(5, 'far fa-circle', 'User Report', 'MISUser', 'UserReport', NULL, NULL, NULL, 'HttpGet', True, True, True, False, 'User Report Menu', NOW(), 'DEVELOPER', False),
+(5, 'far fa-circle', 'Activity', 'MISUser', 'Activity', NULL, NULL, NULL, 'HttpGet', True, True, True, False, 'User Activity Report', NOW(), 'DEVELOPER', False),
+(5, 'far fa-circle', 'Login Report', 'MISUser', 'Login', NULL, NULL, NULL, 'HttpGet', True, True, True, False, 'User Login Report', NOW(), 'DEVELOPER', False),
+(5, 'far fa-circle', 'Maker Checker Report', 'MISUser', 'MakerCheckerReport', NULL, NULL, NULL, 'HttpGet', True, True, True, False, 'Maker Checker Report Details', NOW(), 'DEVELOPER', False),
+(6, 'far fa-circle', 'License Report', 'MISLicense', 'Requests', NULL, NULL, NULL, 'HttpGet', True, True, True, False, 'Report  license requests', NOW(), 'DEVELOPER', False),
+(6, 'far fa-circle', 'Request Movement', 'MISLicense', 'RequestMovement', NULL, NULL, NULL, 'HttpGet', True, True, True, False, 'Request Movement Details', NOW(), 'DEVELOPER', False),
+(6, 'far fa-circle', 'Status Report', 'MISLicense', 'Status', NULL, NULL, NULL, 'HttpGet', True, True, True, False, 'License status Report', NOW(), 'DEVELOPER', False),
+(6, 'far fa-circle', 'Turnaround Time', 'MISLicense', 'TAT', NULL, NULL, NULL, 'HttpGet', True, True, True, False, 'License Turnaround Time Reports ', NOW(), 'DEVELOPER', False),
+(6, 'far fa-circle', 'XML Download', 'MISLicense', 'XMLDownload', NULL, NULL, NULL, 'HttpGet', True, True, True, False, 'XML Download Report', NOW(), 'DEVELOPER', False);
+
+INSERT INTO tbl_password (UserId, PasswordHash, IsActive, ActivationDate, PasswordSalt, SessionId, TransactionDate, PasswordText, IsChangeable)
+VALUES  (1, 'E5zMwlEFWgE=', 1, NOW(),  'nCepJIH7bmBvjDjUeApq/CTv11EnhJDJ', 'DEVELOPER', NOW(), null, False);
+
+INSERT INTO tbl_s_state(Name, IsPositive, IsNegative, IsHold, SessionId, IsActive, TransactionDate) VALUES
+('Approved', True, False, False, 'DEVELOPER', True, NOW()),
+('Rejected', False, True, False, 'DEVELOPER', True, NOW()),
+('RevertBack', False, True, True, 'DEVELOPER', True, NOW()),
+('OnHold', False, False, True, 'DEVELOPER', False, NOW());
+
+INSERT INTO tbl_user (UserTypeId, FullName, UserName, Email, Mobile, Address, Designation, IsActive, SessionId, TransactionDate) 
+VALUES  (1, 'Company Admin', 'admin', 'admin@admin.com',  '9999999999', 'Kolkata, India', null, 1, 'DEVELOPER', NOW());
+
+
+INSERT INTO tbl_usermenu (MenuId, UserId, UserTypeId, IsActive, SessionId, TransactionDate)
+VALUES  (1, 1, 1, 1, 'DEVELOPER', NOW());
+
+
+INSERT INTO tbl_usertype (UserTypeName, UserTypeDetails, IsActive, TransactionDate, SessionId) 
+VALUES ('Super Admin', 'Super Admin', 1, NOW(), 'DEVELOPER');
---
+++ source/LicensingERP.SQL/LicensingERP.TriggerViewsFunctions.sql
@@ -0,0 +1,189 @@
+
+-- TIGGER
+
+-- tg_tbl_request_after_insert
+DROP TRIGGER IF EXISTS tg_tbl_request_after_insert;
+CREATE TRIGGER tg_tbl_request_after_insert
+		AFTER INSERT
+		ON tbl_request
+		FOR EACH ROW
+BEGIN
+	INSERT INTO tbl_request_claim (RequestId, RequestNo,
+	NextUserTypeId, ClaimUserId, ClaimDate,
+	IsActive, AssignDate, TransactionDate, SessionId)
+	  VALUES (NEW.Id, NEW.RequestNo, (SELECT ToUserTypeId FROM tbl_wf_process_assign WHERE WfProcessId = (SELECT Id FROM tbl_wf_process WHERE LicenceTypeId = NEW.LicenceTypeId AND IsInitial = 1) AND FromUserTypeId = NEW.UserTypeId AND StateId = 1), NULL, NULL, 1, NOW(), NOW(), NEW.SessionId);
+END;
+
+
+-- tg_tbl_request_acknowledgement_after_insert
+DROP TRIGGER IF EXISTS tg_tbl_request_acknowledgement_after_insert;
+CREATE TRIGGER tg_tbl_request_acknowledgement_after_insert
+		AFTER INSERT
+		ON tbl_request_acknowledgement
+		FOR EACH ROW
+BEGIN
+	DECLARE p_NextUserTypeId int(11);
+
+	-- UPDATE Old Row
+	UPDATE tbl_request_claim
+	SET IsActive = 0
+	WHERE RequestId = NEW.RequestId
+	AND RequestNo = NEW.RequestNo
+	AND ClaimUserId = NEW.UserId
+	AND NextUserTypeId = NEW.UserTypeId;
+
+
+	  -- ---------------------------------------------------------------
+	  -- Prepare New Row data 
+	  -- if StateId == (IsPositive && !IsNegative && !IsHold)
+	  -- && ProcessId == IsEnd 
+	  -- then Final submit with approve
+	  IF (EXISTS ( SELECT
+		*
+	  FROM tbl_s_state
+	  WHERE IsPositive = 1
+	  AND IsNegative <> 1
+	  AND IsHold <> 1
+	  AND Id = NEW.StateId))
+	AND (EXISTS (SELECT
+		WFProcessId
+	  FROM tbl_wf_process_assign
+	  WHERE WfProcessId IN (SELECT
+		  Id
+		FROM tbl_wf_process
+		WHERE LicenceTypeId = (SELECT
+			LicenceTypeId
+		  FROM tbl_request
+		  WHERE Id = NEW.RequestId)
+		AND IsEnd = 1)
+	  AND FromUserTypeId = NEW.UserTypeId)) THEN
+	INSERT INTO tbl_request_status (RequestId, RequestNo, UserId, UserTypeId, IsApproved,
+	ApproveRejectDate, Remarks, IsActive, SessionId, TransactionDate)
+	  VALUES (NEW.RequestId, NEW.RequestNo, NEW.UserId, NEW.UserTypeId, 1, NOW(), NEW.Remarks, 1, NEW.SessionId, NOW());
+	END IF;
+	  
+
+	  -- ---------------------------------------------------------------
+	  -- if StateId == (IsNegative && !IsHold)
+	  -- && ProcessId == IsEnd 
+	  -- then Final submit with reject
+	  IF (EXISTS ( SELECT
+		*
+	  FROM tbl_s_state
+	  WHERE IsNegative = 1
+	  AND IsHold <> 1
+	  AND Id = NEW.StateId))
+	AND (EXISTS (SELECT
+		WFProcessId
+	  FROM tbl_wf_process_assign
+	  WHERE WfProcessId IN (SELECT
+		  Id
+		FROM tbl_wf_process
+		WHERE LicenceTypeId = (SELECT
+			LicenceTypeId
+		  FROM tbl_request
+		  WHERE Id = NEW.RequestId)
+		AND IsEnd = 1)
+	  AND FromUserTypeId = NEW.UserTypeId)) THEN
+	INSERT INTO tbl_request_status (RequestId, RequestNo, UserId, UserTypeId, IsApproved,
+	ApproveRejectDate, Remarks, IsActive, SessionId, TransactionDate)
+	  VALUES (NEW.RequestId, NEW.RequestNo, NEW.UserId, NEW.UserTypeId, 1, NOW(), NEW.Remarks, 1, NEW.SessionId, NOW());
+	END IF;
+
+
+	-- ---------------------------------------------------------------
+	-- if StateId == IsPositive || (IsNegative && IsHold)
+	-- && ProcessId == (!IsEnd && !IsInitial) 
+	-- Then Move to next lavel
+	SELECT
+	  ToUserTypeId INTO p_NextUserTypeId
+	FROM tbl_wf_process_assign
+	WHERE WfProcessId IN (SELECT
+		Id
+	  FROM tbl_wf_process
+	  WHERE LicenceTypeId = (SELECT
+		  LicenceTypeId
+		FROM tbl_request
+		WHERE Id = NEW.RequestId)
+	  AND (IsInitial <> 1
+	  && IsEnd <> 1))
+	AND FromUserTypeId = NEW.UserTypeId
+	AND StateId = NEW.StateId
+	AND NEW.StateId IN (SELECT
+		Id
+	  FROM tbl_s_state
+	  WHERE IsPositive = 1
+	  OR (IsNegative = 1
+	  AND IsHold = 1));
+
+	  IF p_NextUserTypeId IS NOT NULL 
+		OR p_NextUserTypeId <> 0 THEN
+	-- New Row
+	INSERT INTO tbl_request_claim (RequestId, RequestNo,
+	NextUserTypeId, ClaimUserId, ClaimDate,
+	IsActive, AssignDate, TransactionDate, SessionId)
+	  VALUES (NEW.RequestId, NEW.RequestNo, p_NextUserTypeId, NULL, NULL, 1, NOW(), NOW(), NEW.SessionId);
+	END IF;
+END;
+
+
+
+-- FUNCTION
+
+-- fn_Get_UserFullName
+DROP FUNCTION IF EXISTS fn_Get_UserFullName;
+CREATE FUNCTION fn_Get_UserFullName(p_UserId INT)
+  RETURNS varchar(255) CHARSET utf8
+BEGIN
+DECLARE v_FN varchar(255) DEFAULT '';
+SELECT tu.FullName INTO v_FN FROM tbl_user tu WHERE tu.Id = p_UserId;
+
+RETURN v_FN;
+END;
+
+-- fn_Get_UserTypeName
+DROP FUNCTION IF EXISTS fn_Get_UserTypeName;
+CREATE FUNCTION fn_Get_UserTypeName(p_UserTypeId INT)
+  RETURNS varchar(255) CHARSET utf8
+BEGIN
+  DECLARE v_FN varchar(255) DEFAULT '';
+SELECT tu.UserTypeName INTO v_FN FROM tbl_usertype tu WHERE tu.Id = p_UserTypeId;
+
+RETURN v_FN;
+END;
+
+
+-- Views
+
+-- vw_user_menu
+DROP VIEW IF EXISTS vw_user_menu;
+CREATE VIEW vw_user_menu
+AS
+SELECT
+  tbl_menu_list.Id AS Id,
+  tbl_menu_list.Icon AS Icon,
+  tbl_menu_list.DisplayText AS DisplayText,
+  tbl_menu_list.Controller AS Controller,
+  tbl_menu_list.MenuGroupId AS MenuGroupId,
+  tbl_menu_list.ActionResult AS ActionResult,
+  tbl_menu_list.RouteId AS RouteId,
+  tbl_menu_list.QueryString AS QueryString,
+  tbl_menu_list.HttpType AS HttpType,
+  tbl_menu_list.IsActive AS IsActive,
+  tbl_menu_list.IsDisplayable AS IsDisplayable,
+  tbl_menu_list.IsForAdmin AS IsForAdmin,
+  tbl_menu_list.Description AS Description,
+  tbl_menu_list.IsForWorkflow AS IsForWorkflow
+FROM (tbl_menu_list
+  JOIN tbl_usermenu
+    ON ((tbl_menu_list.Id = tbl_usermenu.MenuId)));
+	
+	
+-- Procedure
+
+
+
+
+
+
+
