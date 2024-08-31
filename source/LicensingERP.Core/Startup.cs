eb7f436c7fbc34c7e8a099a53b58a3c4e10eb6de
Soubhik Nandy <nandy.soubhik@live.co.uk>
2024-08-31 10:02:56 +0530

    Script added

--- source/LicensingERP.Core/Startup.cs
+++ source/LicensingERP.Core/Startup.cs
@@ -137,9 +137,9 @@ namespace LicensingERP
                 {
                     options.Run(async context =>
                     {
-                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.StatusCode = context.Response.StatusCode;// (int)HttpStatusCode.InternalServerError;
                         context.Response.ContentType = "text/html";
-                        var ex = context.Features.Get<IExceptionHandlerFeature>();
+                        var ex = context.Features.Get<IExceptionHandlerFeature>();                        
                         if (ex != null)
                         {
                             var err = $"<h1>Error: {ex.Error.Message}</h1>{ex.Error.StackTrace}";
